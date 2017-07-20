cf69125fd5d3e158c66116d471c67ab39a6428a4
jooyon-PC\jooyon <l.s.b3422>
2017-07-21 03:52:54 +0900

    장르별 출력 오류 수정

--- shoptest4/src/wm/model/dao/ProductDAOImpl.java
+++ shoptest4/src/wm/model/dao/ProductDAOImpl.java
@@ -539,8 +539,35 @@ public class ProductDAOImpl implements ProductDAO {
 }
 	@Override
 	public List<ProductDTO> productGenre(String genre) throws SQLException {
-		// TODO Auto-generated method stub
-		return null;
+		 Connection con = null;
+         PreparedStatement ps = null;
+         ResultSet rs = null;
+         List<ProductDTO> list = new ArrayList<>();
+         try{
+            con = DBUtil.getConnection();
+             String sql = "select * from Product where p_genre=?";
+            // where 議곌굔�쑝濡� �옣瑜대퀎, 媛�寃⑹닚!
+             ps = con.prepareStatement(sql);
+             ps.setString(1, genre);
+             rs = ps.executeQuery();
+                while(rs.next()){
+                   list.add(new ProductDTO(
+                         rs.getInt(1),
+                         rs.getString(2),
+                         rs.getString(3),
+                         rs.getInt(4),
+                         rs.getString(5),
+                         rs.getString(6),
+                         rs.getInt(7),
+                         rs.getString(8),
+                         rs.getInt(9),
+                         rs.getString(10),
+                         rs.getString(11)));
+                            }
+             }finally{
+                DBUtil.dbClose(con, ps,rs);
+             }
+          return list;   
 	}
 	@Override
 	   
